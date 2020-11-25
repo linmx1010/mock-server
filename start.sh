97e27e2a9b9445d316eef54fbb19c2b38588e6eb
unknown <781183284@qq.com>
2020-11-25 19:02:12 +0800

    first commit

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-nohup java -jar ./moco-runner-0.12.0-standalone.jar start -p 56789 -c foo.json >./test.log 2>&1 &
+nohup java -jar ./moco-runner-1.1.2-standalone.jar start -p 56789 -c foo.json >./test.log 2>&1 &
